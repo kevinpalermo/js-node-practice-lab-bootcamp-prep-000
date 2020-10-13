08f85dc9c5235eecbc287cf44fea4abaa26553c4
Kevin Palermo <kevpalermo@gmail.com>
2020-10-13 19:27:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // write your code below!
 
 var name = "Kevin"
+<<<<<<< HEAD
 var height = 65
+=======
+var height = "74"
+>>>>>>> 0f92f7a3c1ae8617b8ac2fce4cf699d589f43718
 
 console.log("Name:")
 console.log(name)
